cf7beeb8ddf1f11b9a7b138ff0181bb5c80514c7
GengGode <GengGode@outlook.com>
2022-12-03 03:04:43 +0800

    添加项目文件。

---
+++ WallpaperPreview/WallpaperPreview/ImageFileInfo.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <winrt/Microsoft.UI.Xaml.Media.Imaging.h>
+#include <winrt/Windows.UI.Xaml.Data.h>
+
+//#include <winrt/Windows.System.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Storage.FileProperties.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.System.Threading.h>
+
+
+
+//#include <winrt/Windows.Storage.Streams.h>
+//#include <winrt/Windows.Foundation.h>
+//#include <winrt/Windows.Storage.h>
+//#include <winrt/Windows.Graphics.Imaging.h>
+
+using namespace winrt::Microsoft::UI::Xaml::Media::Imaging;
+using namespace winrt::Windows::UI::Xaml::Data;
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::FileProperties;
+using namespace winrt::Windows::Storage::Streams;
+using namespace winrt::Windows::Foundation;
+
+
+namespace winrt::WallpaperPreview::implementation
+{
+	class ImageFileInfo : public winrt::implements<ImageFileInfo, winrt::Windows::UI::Xaml::Data::INotifyPropertyChanged>
+	{
+	public:
+		ImageFileInfo(ImageProperties const& properties, StorageFile const& imageFile, winrt::hstring  const& name, winrt::hstring const& type);
+	public:
+		StorageFile ImageFile();
+		ImageProperties Properties();	
+	public:
+		// C#: public async Task<BitmapImage> GetImageSourceAsync()
+		// C++: winrt::Windows::Foundation::IAsyncOperation<winrt::Microsoft::UI::Xaml::Media::Imaging::BitmapImage> GetImageSourceAsync();
+		IAsyncOperation<BitmapImage> GetImageSourceAsync();
+	public:
+		// C#: public async Task<BitmapImage> GetImageThumbnailAsync()
+		// C++: winrt::Windows::Foundation::IAsyncOperation<winrt::Microsoft::UI::Xaml::Media::Imaging::BitmapImage> GetImageThumbnailAsync();
+		IAsyncOperation<BitmapImage> GetImageThumbnailAsync();
+	public:
+		winrt::hstring ImageName();
+		winrt::hstring ImageFileType();
+	public:	
+		// C#: public string ImageDimensions => $"{ImageProperties.Width} x {ImageProperties.Height}";
+		// C++: winrt::hstring ImageDimensions() { return winrt::to_hstring(m_imageProperties.Width()) + L" x " + winrt::to_hstring(m_imageProperties.Height()); }
+		winrt::hstring ImageDimensions() { return winrt::to_hstring(m_imageProperties.Width()) + L" x " + winrt::to_hstring(m_imageProperties.Height()); }
+	public:
+		winrt::hstring ImageTitle();
+		void ImageTitle(winrt::hstring const& value);
+	public:
+		int ImageRating();
+		void ImageRating(int const& value);
+	public:
+		// C#: public event PropertyChangedEventHandler PropertyChanged;
+		// C++: winrt::event_token PropertyChanged(Windows::UI::Xaml::Data::PropertyChangedEventHandler const& value);
+		winrt::event_token PropertyChanged(PropertyChangedEventHandler const& value);
+		void PropertyChanged(winrt::event_token const& token);
+			
+
+	protected:
+		// C#: protected void OnPropertyChanged([CallerMemberName] string propertyName = null) = >PropertyChanged ? .Invoke(this, new PropertyChangedEventArgs(propertyName));
+		// C++: void OnPropertyChanged(winrt::hstring const& propertyName) { PropertyChanged(*this, PropertyChangedEventArgs(propertyName)); }
+		void OnPropertyChanged(winrt::hstring const& propertyName = L"");
+
+
+	private:
+		StorageFile m_imageFile{ nullptr };
+		ImageProperties m_imageProperties{ nullptr };
+		winrt::hstring m_iamgeName;
+		winrt::hstring m_imageFileType;
+		winrt::event<PropertyChangedEventHandler> m_propertyChanged;
+	};
+}
---
+++ WallpaperPreview/WallpaperPreview/MainWindow.xaml.h
@@ -0,0 +1,139 @@
+﻿#pragma once
+#include "MainWindow.g.h"
+#include "ImageFileInfo.h"
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Storage.Search.h>
+
+using namespace winrt::Windows::ApplicationModel;
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::Search;
+
+namespace winrt::WallpaperPreview::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+		// C#: public ObservableCollection<ImageFileInfo> Images { get; } = new ObservableCollection<ImageFileInfo>();
+		// C++: winrt::Windows::UI::Xaml::Interop::IBindableObservableVector Images() { return m_images; }
+		winrt::Windows::UI::Xaml::Interop::IBindableObservableVector Images() { return m_images; }
+		winrt::Windows::UI::Xaml::Interop::IBindableObservableVector m_images;
+		
+		IAsyncAction MainWindow::GetItemsAsync()
+		{
+			StorageFolder appInstalledFolder = Package::Current().InstalledLocation();
+			StorageFolder picturesFolder = co_await appInstalledFolder.GetFolderAsync(L"Assets\\Samples");
+
+			// C#: var result = picturesFolder.CreateFileQueryWithOptions(new QueryOptions());
+			// C++: auto result = picturesFolder.CreateFileQueryWithOptions(QueryOptions());
+			auto result = picturesFolder.CreateFileQueryWithOptions(QueryOptions());
+		
+			// C#: IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
+			// C++: auto imageFiles = co_await result.GetFilesAsync();
+			auto imageFiles = co_await result.GetFilesAsync();
+		
+			for (auto imageFile : imageFiles)
+			{
+				Images().Append(winrt::make<ImageFileInfo>(imageFile));
+			}
+
+			// C#: ImageGridView.ItemsSource = Images;
+			// C++: ImageGridView().ItemsSource(Images());
+			ImageGridView().ItemsSource(Images());
+		}
+
+		static IAsyncOperation<ImageFileInfo> LoadImageInfo(StorageFile file)
+		{
+			// C#: var properties = await file.Properties.GetImagePropertiesAsync();
+			// C++: auto properties = co_await file.Properties().GetImagePropertiesAsync();
+			auto properties = co_await file.Properties().GetImagePropertiesAsync();
+			// C#: ImageFileInfo info = new(properties, file, file.DisplayName, file.DisplayType);
+			// C++: auto info = winrt::make<ImageFileInfo>(properties, file, file.DisplayName(), file.DisplayType());
+			auto info = winrt::make<ImageFileInfo>(properties, file, file.DisplayName(), file.DisplayType());
+			
+			co_return info;
+		}
+    };
+	
+}
+
+namespace winrt::WallpaperPreview::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
+
+
+/* C#:
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.Search;
+...
+private async Task GetItemsAsync()
+{
+    StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
+    StorageFolder picturesFolder = await appInstalledFolder.GetFolderAsync("Assets\\Samples");
+
+    var result = picturesFolder.CreateFileQueryWithOptions(new QueryOptions());
+
+    IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
+    foreach (StorageFile file in imageFiles)
+    {
+        Images.Add(await LoadImageInfo(file));
+    }
+
+    ImageGridView.ItemsSource = Images;
+}
+
+public async static Task<ImageFileInfo> LoadImageInfo(StorageFile file)
+{
+    var properties = await file.Properties.GetImagePropertiesAsync();
+    ImageFileInfo info = new(properties,
+                             file, file.DisplayName, file.DisplayType);
+
+    return info;
+}
+*/
+
+/* C++:
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#include "ImageFileInfo.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Storage;
+using namespace Windows::Storage::Search;
+
+IAsyncOperation<ImageFileInfo> LoadImageInfo(StorageFile file)
+{
+	ImageProperties properties = co_await file.Properties().GetImagePropertiesAsync();
+	ImageFileInfo info = { properties, file, file.DisplayName(), file.DisplayType() };
+
+	co_return info;
+}
+
+IAsyncAction MainWindow::GetItemsAsync()
+{
+	StorageFolder appInstalledFolder = Package::Current().InstalledLocation();
+	StorageFolder picturesFolder = co_await appInstalledFolder.GetFolderAsync(L"Assets\\Samples");
+
+	QueryOptions options;
+	StorageFileQueryResult result = picturesFolder.CreateFileQueryWithOptions(options);
+
+	IReadOnlyList<StorageFile> imageFiles = co_await result.GetFilesAsync();
+	for (StorageFile file : imageFiles)
+	{
+		Images().Append(co_await LoadImageInfo(file));
+	}
+
+	ImageGridView().ItemsSource(Images());
+}
+*/
+
